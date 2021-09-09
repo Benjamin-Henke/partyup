5bb826fbbad76f1d543b6dd40ea9b03b673621a4
Benjamin Henke <benjaminmhenke@gmail.com>
2021-09-09 15:56:19 -0500

    Users can only see their data

--- server/routes/myParties.router.js
+++ server/routes/myParties.router.js
@@ -6,8 +6,22 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    let sqlText = `SELECT * FROM "parties"`;
-    pool.query(sqlText).then(result => {
+    let sqlText, sqlParams;
+
+    if (req.user.authLevel === 'ADMIN') {
+        queryText = `SELECT * FROM "pet"`;
+        queryParams = [];
+    }
+    else {
+        // Regular users see only their pets
+        sqlText = `
+        SELECT * FROM "parties"
+        WHERE user_id = $1
+        `;
+        sqlParams = [req.user.id]
+    }
+
+    pool.query(sqlText, sqlParams).then(result => {
         console.log('My Parties response', result.rows);
         res.send(result.rows);
     }).catch(error => {

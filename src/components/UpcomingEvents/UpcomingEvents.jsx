59826d62ddeb8d1f9a58aaccd42358014cdd2ab3
Benjamin Henke <benjaminmhenke@gmail.com>
2021-09-13 13:36:29 -0500

    some styling

--- src/components/UpcomingEvents/UpcomingEvents.jsx
+++ src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import './UpcomingEvents.css';
+
 export default function RecentPosts () {
     // Set hook variables
     const dispatch = useDispatch();
@@ -25,8 +27,10 @@ export default function RecentPosts () {
                 <div class="card">
                     <h5 class="card-header">{events.board_game}</h5>
                     <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">{events.location}</h6>
-                        <a href="#" class="btn btn-secondary">Go somewhere</a>
+                        <h6 id="loc" class="card-subtitle mb-2 text-muted">{events.location}</h6>
+                        <a href="#" class="btn btn-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" /><path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" /></svg>
+                        </a>
                     </div>
                 </div>
             )}

850702e47752415296b67d5396b944433199bdd1
Benjamin Henke <benjaminmhenke@gmail.com>
2021-09-18 12:15:12 -0500

    Data from api inserts into db successfully

--- server/routes/createParty.router.js
+++ server/routes/createParty.router.js
@@ -12,43 +12,45 @@ router.post('/', rejectUnauthenticated,(req, res) => {
     console.log('Info to insert into DB', req.body);
     
 
-    // const sqlText = `
-    //     INSERT INTO "parties" ("board_game", "number_of_players", "experience", "location", "date_time", "owner_id")
-    //     VALUES ($1, $2, $3, $4, $5, $6)
-    //     RETURNING "id";
-    //     `;
+    const sqlText = `
+        INSERT INTO "parties" ("board_game", "number_of_players", "experience", "location", "date_time", "image", "description", "owner_id")
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING "id";
+        `;
 
-    // const sqlParams = [
-    //     req.body.boardGame,
-    //     req.body.numberOfPlayers,
-    //     req.body.experience,
-    //     req.body.location,
-    //     req.body.dateTime,
-    //     req.user.id,
-    // ];
+    const sqlParams = [
+        req.body.userInputs.boardGame,          // $1
+        req.body.userInputs.numberOfPlayers,    // $2
+        req.body.userInputs.experience,         // $3
+        req.body.userInputs.location,           // $4
+        req.body.userInputs.dateTime,           // $5
+        req.body.description,                   // $6
+        req.body.image,                         // $7
+        req.user.id,                            // $8
+    ];
 
-    // // First Query makes the party
-    // pool.query(sqlText, sqlParams).then(result => {
-    //     console.log('New Party Id:', result.rows[0].id); // ID of the created party
+    // First Query makes the party
+    pool.query(sqlText, sqlParams).then(result => {
+        console.log('New Party Id:', result.rows[0].id); // ID of the created party
 
-    // const insertIntoParty = `
-    //     INSERT INTO "users_parties" ("users_id", "parties_id")
-    //     VALUES ($1, $2)
-    // `;
+    const insertIntoParty = `
+        INSERT INTO "users_parties" ("users_id", "parties_id")
+        VALUES ($1, $2)
+    `;
 
-    // const insertOwnerAndPartyId = [
-    //     req.user.id,            // $1
-    //     result.rows[0].id     // $2
-    // ]
+    const insertOwnerAndPartyId = [
+        req.user.id,            // $1
+        result.rows[0].id     // $2
+    ]
 
-    // // Second Query inserts the owner into the number of players ("users_parties" table) 
-    //     pool.query(insertIntoParty, insertOwnerAndPartyId).then(result => {
-    //         res.sendStatus(201)
-    //     })
-    // }).catch(error => {
-    //     console.error('Create Party Post Error', error);
-    //     res.sendStatus(500);
-    // })
+    // Second Query inserts the owner into the number of players ("users_parties" table) 
+        pool.query(insertIntoParty, insertOwnerAndPartyId).then(result => {
+            res.sendStatus(201)
+        })
+    }).catch(error => {
+        console.error('Create Party Post Error', error);
+        res.sendStatus(500);
+    })
 });
 
 module.exports = router;

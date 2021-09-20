fb1e668a62e7ae931a701952f67e84b8e9ed5b34
Benjamin Henke <benjaminmhenke@gmail.com>
2021-09-20 09:29:07 -0500

    Styling

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,7 @@
 export default function WelcomePage() {
     return (
         <div id="welcomeMessage">
-            <h4>START SEARCHING FOR GAMES TO JOIN AND PLAY!</h4>
+            <h3 id="WelcomeMessage">START SEARCHING FOR GAMES TO JOIN AND PLAY!</h3>
             <img 
                 src="http://pngimg.com/uploads/dice/dice_PNG113.png"
                 height="780px"
